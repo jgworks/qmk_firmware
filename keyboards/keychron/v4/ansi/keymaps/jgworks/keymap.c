fec1f882f3ef097d87d286e5f30c82737b11aff1
Jonathan G. Works <jgworks@gmail.com>
2025-11-01 06:23:57 -0400

    Update layer FN3 to have a delete key

--- keyboards/keychron/v4/ansi/keymaps/jgworks/keymap.c
+++ keyboards/keychron/v4/ansi/keymaps/jgworks/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______),
 
     [_FN3] = LAYOUT_60_ansi(
-        KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,
+        KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DELETE,
         RM_TOGG, RM_NEXT,  RM_VALU, RM_HUEU, RM_SATU, RM_SPDU, _______, _______, _______, _______, _______,  _______,  _______,  _______,
         _______, RM_PREV,  RM_VALD, RM_HUED, RM_SATD, RM_SPDD, _______, _______, _______, _______, _______,  _______,            _______,
         _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
